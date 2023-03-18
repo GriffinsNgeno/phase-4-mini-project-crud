735a57297dbb6c7a3441f1738319db516158108d
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-18 14:47:14 +0300

    Completed Lab

--- spec/requests/plants_spec.rb
+++ spec/requests/plants_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Spices", type: :request do
+RSpec.describe "Plants", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
